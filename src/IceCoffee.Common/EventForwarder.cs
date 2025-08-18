f04f9a40534307bc0d767740f5ffa75327c273d2
IceCoffee <1249993110@qq.com>
2025-08-18 09:44:00 +0800

    Updated version and added an event forwarder class
    In IceCoffee.Common.csproj , the version number was updated from 1.3.2 to 1.3.3, and the versions of Microsoft.Extensions.ObjectPool and System.Threading.Channels were updated. Additionally, a reference to System.Reflection.Emit.Lightweight was added for the netstandard2.0 framework.
    
    In EventForwarder.cs , a new EventForwarder class was added to dynamically subscribe to events and call the OnEventRaised method when an event is triggered. This class obtains event information through reflection and uses a dynamic method to create an event handler to correctly handle event parameters.

---
+++ src/IceCoffee.Common/EventForwarder.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IceCoffee.Common
+{
+    public class EventForwarder
+    {
+        public event Action<string, EventArgs>? EventRaised;
+
+        public EventForwarder(object instance)
+        {
+            SubscribeAllEvents(instance);
+        }
+
+        private void SubscribeAllEvents(object instance)
+        {
+            var events = instance.GetType().GetEvents();
+            foreach (var evt in events)
+            {
+                var handler = CreateEventHandler(evt);
+                evt.AddEventHandler(instance, handler);
+            }
+        }
+
+        private void OnEventRaised(string eventName, EventArgs eventArgs)
+        {
+            EventRaised?.Invoke(eventName, eventArgs);
+        }
+
+        private Delegate CreateEventHandler(EventInfo eventInfo)
+        {
+            var handlerType = eventInfo.EventHandlerType!;
+            var invokeMethod = handlerType.GetMethod("Invoke")!;
+            var parameters = invokeMethod.GetParameters();
+
+            if (parameters.Length != 2)
+                throw new InvalidOperationException("The number of event delegate parameters is not 2");
+
+            if (parameters[0].ParameterType != typeof(object))
+                throw new InvalidOperationException("The first parameter is not of type object");
+
+            var eventArgsType = parameters[1].ParameterType;
+            if (typeof(EventArgs).IsAssignableFrom(eventArgsType) == false)
+                throw new InvalidOperationException("The second argument is not an EventArgs or subclass thereof");
+
+            // Since DynamicMethod is a static method, there is no this, and the instance needs to be bound through CreateDelegate
+            // But here we can't directly access the instance, so we use the closure target to bind the instance
+            var parameterTypes = new Type[] { typeof(EventForwarder), typeof(object), eventArgsType };
+
+            string eventName = eventInfo.Name;
+
+            // Define a dynamic method with the same signature as the event delegate and return void
+            var dm = new DynamicMethod(
+                $"DynamicHandler_{eventName}",
+                typeof(void),
+                parameterTypes,
+                typeof(EventForwarder).Module,
+                skipVisibility: true);
+
+            var il = dm.GetILGenerator();
+
+            // Load the closure instance: this (Parameter 0)
+            il.Emit(OpCodes.Ldarg_0);
+
+            // Loading event name string: eventName (Parameter 1)
+            il.Emit(OpCodes.Ldstr, eventName);
+
+            // Loading event parameter: eventArgs (Parameter 2)
+            il.Emit(OpCodes.Ldarg_2);
+
+            // Calling instance methods: OnEventRaised(object, EventArgs)
+#if NETCOREAPP
+            var onEventRaisedMethod = typeof(EventForwarder)
+                .GetMethod(nameof(OnEventRaised), BindingFlags.NonPublic | BindingFlags.Instance, types: new Type[] { typeof(string), typeof(EventArgs) })!;
+#else
+            var onEventRaisedMethod = typeof(EventForwarder)
+                .GetMethod(nameof(OnEventRaised), BindingFlags.NonPublic | BindingFlags.Instance, null, new Type[] { typeof(string), typeof(EventArgs) }, null)!;
+#endif
+            il.Emit(OpCodes.Callvirt, onEventRaisedMethod);
+
+            il.Emit(OpCodes.Ret);
+
+            // Create a delegate and bind the instance to a closure
+            var del = dm.CreateDelegate(handlerType, this);
+            return del;
+        }
+    }
+}
